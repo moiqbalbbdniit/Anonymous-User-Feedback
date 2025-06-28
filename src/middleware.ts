0a4a54ff8b80d4a8aa2ca9f11ff354803250fe9c
Mohammad Iqbal <ki925053@gmail.com>
2025-06-28 20:11:44 +0530

    fix the verify route error

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { getToken } from 'next-auth/jwt';
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
+  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', ],
 };
 
 export async function middleware(request: NextRequest) {
@@ -16,7 +16,7 @@ export async function middleware(request: NextRequest) {
     token &&
     (url.pathname.startsWith('/sign-in') ||
       url.pathname.startsWith('/sign-up') ||
-      url.pathname.startsWith('/verify') ||
+      
       url.pathname === '/')
   ) {
     return NextResponse.redirect(new URL('/dashboard', request.url));
